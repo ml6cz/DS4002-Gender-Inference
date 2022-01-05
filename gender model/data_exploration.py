3ea6186068ab4fa9c25e0825cecb81728daa1fec
Emmye Mullins <89602156+Emmyemullins@users.noreply.github.com>
2022-01-04 19:18:26 -0500

    Update data_exploration.py

--- gender model/data_exploration.py
+++ gender model/data_exploration.py
@@ -9,3 +9,5 @@ df = pd.DataFrame(files, columns = ['Age', 'Gender', 'Race', 'Time Taken'])
 print(df)
 
 df2 = df.loc[(df['Gender'] == '0') | (df['Gender'] == '1')]
+df2.loc[:, 'Age'] = df2['Age'].astype(int)
+df2 = df2.loc[(df2['Age'] >= 0) & (df2['Age'] <= 116)]
