28a82ee4809c3ed9b0801bde53b80053843ce660
Megan Lin <megan.w.lin@gmail.com>
2022-01-04 21:20:00 -0500

    Add visualizations for data deep dive

--- gender model/data_exploration.py
+++ gender model/data_exploration.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
+import matplotlib.pyplot as plt
 
 mypath = "/Users/megan/Documents/4th Year/DS 4002/Week 1/gender model/crop_images"
 files = [f.split("_") for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -16,22 +17,36 @@ df2 = df2[df2.Age.apply(lambda x: x.isnumeric())]
 df2 = df2[df2.Gender.apply(lambda x: x.isnumeric())]
 df2 = df2[df2.Race.apply(lambda x: x.isnumeric())]
 
+#convert all columns to numeric values
 df2["Age"] = pd.to_numeric(df2["Age"])
 df2["Gender"] = pd.to_numeric(df2["Gender"])
 df2["Race"] = pd.to_numeric(df2["Race"])
 
+print(len(df.index)-len(df2.index), "pictures were removed from the dataset")
+
+#map numeric values to their UTK counterpart
 gendermap = {0: "Male", 1: "Female"}
 racemap = {0: "White", 1: "Black", 2: "Asian", 3: "Indian", 4: "Other"}
 df2 = df2.replace({"Gender": gendermap, "Race": racemap})
-print(df2)
 
-print(len(df.index)-len(df2.index), "pictures were removed from the dataset")
+#print a numeric summary of the age column as it is the only numeric column
+print(df2.describe())
 
-#remove any rows that are outside of the UTK ranges
-#df2 = df.loc[(df['Gender'] == '0') | (df['Gender'] == '1')]
-#df2.loc[:, 'Age'] = df2['Age'].astype(int)
-#df2 = df2.loc[(df2['Age'] >= 0) & (df2['Age'] <= 116)]
+#Manual Check the Distributions
+print(df2.groupby(by='Gender').agg('count'))
+print(df2.groupby(by='Race').agg('count'))
 
-#df3 = df2
-#bins= [0]+list(range(4,85,5))+[116]
-#df3['Age Range'] = pd.cut(df3['Age'], bins=bins, right=False)
+
+#Plotting all three distributions
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+ax1.hist(df2["Age"])
+ax1.set_title("Age Distribution")
+ax1.set_xlabel('Ages')
+
+ax2.bar(["Female", "Male"], df2["Gender"].value_counts())
+ax2.set_title("Gender Distribution")
+ax2.set_xlabel('Gender')
+
+df2['Race'].value_counts().plot(kind='bar')
+ax3.set_title("Race Distribution")
+plt.show()
