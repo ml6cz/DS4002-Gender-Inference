eb22afc27cc08f3f36c908419e43385d0dd98878
Megan Lin <megan.w.lin@gmail.com>
2022-01-06 14:57:36 -0500

    Model creation with 50 epochs

--- gender model/gender_model_maker.py
+++ gender model/gender_model_maker.py
@@ -2,6 +2,13 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import os
+import matplotlib.pyplot as plt
+import keras
+#from tensorflow.keras import plot_model
+import pydot
+import pydotplus
+import graphviz
+
 os.chdir("/Users/megan/Documents/4th Year/DS 4002/Week 1/gender model")
 
 image_size = (180, 180)
@@ -23,3 +30,111 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=image_size,
     batch_size=batch_size,
 )
+
+# Data Augmentation
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+)
+
+#Preprocess the data
+augmented_train_ds = train_ds.map(
+  lambda x, y: (data_augmentation(x, training=True), y))
+
+# Configure for performance
+train_ds = train_ds.prefetch(buffer_size=32)
+val_ds = val_ds.prefetch(buffer_size=32)
+
+# Build the model
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = data_augmentation(inputs)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+#keras.utils.plot_model(model, show_shapes=True)
+# 
+# # Train the Model
+epochs = 50
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+)
+
+images=["Gender/Female/4_1_2_20161219141808855.jpg.chip.jpg",
+        "Gender/Female/7_1_4_20161224001014334.jpg.chip.jpg",
+        "Gender/Female/10_1_0_20170109204255055.jpg.chip.jpg",
+        "Gender/Male/4_0_0_20170109192342033.jpg.chip.jpg",
+        "Gender/Male/8_0_0_20170110220416458.jpg.chip.jpg",
+        "Gender/Male/26_0_2_20161219191301043.jpg.chip.jpg"]
+
+for img_path in images:
+    img = keras.preprocessing.image.load_img(
+        img_path, target_size=image_size
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    score = predictions[0]
+    print(
+        "This image is %.2f percent cat and %.2f percent dog."
+        % (100 * (1 - score), 100 * score)
+    )
